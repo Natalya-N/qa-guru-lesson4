d6a8736f63d02a44ee32d7e2371adc038e18b7e2
Нечаева Наталья Владимировна <nechaevanatascha@yandex.ru>
2025-01-08 22:23:07 +0300

    added test to check junit example

--- src/test/java/CheckJUnitExampleTest.java
+++ src/test/java/CheckJUnitExampleTest.java
@@ -5,7 +5,7 @@ import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
-public class CheckJUnitExample {
+public class CheckJUnitExampleTest {
 
     @BeforeAll
     static void setup() {
